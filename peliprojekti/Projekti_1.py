305015ad84fca266fbacc4d098f2dff2f43e927f
Marta <netrebamarta90@gmail.com>
2026-08-30 12:45:24 +0300

    Done Project 1 and Project 2

---
+++ peliprojekti/Projekti_1.py
@@ -0,0 +1,31 @@
+nimi = input("Anna nimesi: ")
+ikä = int(input("Anna ikä: "))
+
+print(f"Nimesi: {nimi}")
+print(f"Ikä: {ikä}")
+
+if ikä >= 12:
+    print("Tervetuloa pelaamaan, " + nimi + "!")
+
+    peli_käynnissä = True
+    while peli_käynnissä:
+        print("\n--- PÄÄVALIKKO ---")
+        print("Komennot: pelaa, pisteet, lopeta")
+        
+        komento = input("Syötä komento: ")
+        
+        if komento == "lopeta":
+            print("Kiitos pelaamisesta! Ohjelma sammuu.")
+
+            peli_käynnissä = False 
+        elif komento == "pelaa":
+            print("Peli alkaa... Valmistaudu!")
+            
+        elif komento == "pisteet":
+            print("Ennätyspisteesi ovat: 100 pistettä.")
+            
+        else:
+            print("Tuntematon komento. Yritä uudelleen.")
+
+else:
+    print("Olet alaikäinen. Ohjelma sammuu.")
